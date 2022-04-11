763b0c4a12989e596eb71e21bd3133cfbf167f0e
msd <sudhans@gmail.com>
2022-04-11 21:50:54 +0530

    - Updated Material Library to 1.6 beta to get always visible labels for Range Sliders

--- app/src/main/kotlin/com/msd/rentalsearch/ui/RentPerWeekFragment.kt
+++ app/src/main/kotlin/com/msd/rentalsearch/ui/RentPerWeekFragment.kt
@@ -8,8 +8,11 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
+import com.google.android.material.slider.LabelFormatter
+import com.google.android.material.slider.RangeSlider
 import com.msd.rentalsearch.R
 import com.msd.rentalsearch.presentation.viewmodel.SharedSearchViewModel
+import java.util.*
 
 class RentPerWeekFragment : Fragment(){
 
@@ -24,6 +27,10 @@ class RentPerWeekFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        view.findViewById<RangeSlider>(R.id.rangeslider_rentperweek).setLabelFormatter { value ->
+            String.format(Locale.US, "%.0f INR", value)
+        }
+
         view.findViewById<Button>(R.id.button_next)?.setOnClickListener {
             viewModel.printHello()
             val action = RentPerWeekFragmentDirections.actionRentPerWeekFragmentToSearchResultFragment()
